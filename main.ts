f6341820448e629bfe76224671ccd829123eac18
Joseph Essel Moses <jmosesessel21@gmail.com>
2021-09-21 14:31:37 +0000

    initial commit

---
+++ main.ts
@@ -0,0 +1,10 @@
+import './style.scss'
+//import styles from './example.module.css'
+import { debounce } from 'lodash-es'
+
+console.log(debounce)
+
+document.querySelector('#app').innerHTML = `
+  <h1 class="">Hello Vite!</h1>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+`
